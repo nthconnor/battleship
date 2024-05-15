48902067c01bc590f83c2cf1ce72205d923f3b4a
Connor Smith <134646469+nthconnor@users.noreply.github.com>
2024-05-15 13:53:29 -0400

    pathing fix

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ class Game {
     this.elements.buttons.rulesButton.style.display = "none";
     this.elements.title.style.visibility = "hidden";
     this.elements.body.style.backgroundImage =
-      "url(../assets/backgrounds/storm_gif2.gif)";
+      "url(assets/backgrounds/storm_gif2.gif)";
     this.elements.audio.soundtrack.play();
     this.elements.audio.ambience.play();
     this.elements.audio.startButton.play();
